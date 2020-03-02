cb982b446d70bad0740b6386e656bfac0a711581
Ali BARIN <ali.barin53@gmail.com>
2020-03-02 16:59:08 +0100

    feat: Add ExternalScripts client

--- test/rest/Apidaze.js
+++ test/rest/Apidaze.js
@@ -28,7 +28,7 @@ test('has properties such as apiKey, apiSecret and apiUrl', t => {
 test('has various clients as a property', t => {
   const client = new Apidaze(API_KEY, API_SECRET);
 
-  const properties = [client.misc];
+  const properties = [client.misc, client.messages, client.externalScripts];
 
   properties.forEach(property => {
     t.is(typeof property, 'object');
